eafa7ff6420bb71c006ff1f5c9229c3629552ba7
Evgenia Starodubtseva <starodubtseva_0520@mail.ru>
2025-02-26 15:07:23 +0300

    homework_26.02.2025

--- js/main.js
+++ js/main.js
@@ -1,67 +1,3 @@
-// Задание 2
-// // Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом. Например:
-// ucFirst("вася") == "Вася";
-// function ucFirst(str) {
-//   if (!str) return str;
-//   return str[0].toUpperCase() + str.slice(1);
-// }
-// console.log(ucFirst("Вася"));
-
-// Задание 3
-// // Есть стоимость в виде строки "$120". То есть сначала идёт знак валюты, а затем – число.
-// // Создайте функцию extractCurrencyValue(str), которая будет из такой строки выделять числовое значение и возвращать его.
-// function extractCurrencyValue(str) {
-//   return str.slice(1);
-// }
-// console.log(extractCurrencyValue("$120"));
-
-// Задание 4
-// // Давайте произведём 5 операций с массивом.
-// // Создайте массив styles с элементами «Джаз» и «Блюз».
-// // Добавьте «Рок-н-ролл» в конец.
-// // Замените значение в середине на «Классика». Ваш код для поиска значения в середине должен работать для массивов с любой длиной.
-// // Удалите первый элемент массива и покажите его.
-// // Вставьте Рэп и Регги в начало массива.
-// let styles = ["Джаз", "Блюз"];
-// styles.push("Рок-н-ролл");
-// console.log(styles.shift());
-// styles.unshift("Рэп", "Регги");
-
-// Задание 5
-// let arr = [5, 2, 1, -10, 8];
-// arr.sort((x, y) => y - x);
-// console.log(arr); // 8, 5, 2, 1, -10
-
-// Задание 6
-// // У вас есть массив объектов user, и в каждом из них есть user.name. Напишите код, который преобразует их в массив имён.
-// // Например:
-// let vasya = { name: "Вася", age: 25 };
-// let petya = { name: "Петя", age: 30 };
-// let masha = { name: "Маша", age: 28 };
-
-// let users = [vasya, petya, masha];
-
-// let names = users.map((items) => items.name);
-// console.log(names); // Вася, Петя, Маша
-
-// Задание 7
-// // Напишите функцию sortByAge(users), которая принимает массив объектов со свойством age и сортирует их по нему.
-// function sortByAge(users) {
-//   arr.sort((x, y) => x.age - b.age);
-// }
-// let vasya = { name: "Вася", age: 25 };
-// let petya = { name: "Петя", age: 30 };
-// let masha = { name: "Маша", age: 28 };
-
-// let arr = [vasya, petya, masha];
-
-// sortByAge(arr);
-
-// // теперь: [vasya, masha, petya]
-// coonsle.log(arr[0].name); // Вася
-// coonsle.log(arr[1].name); // Маша
-// coonsle.log(arr[2].name); // Петя
-
 // 1.  Задача: Напишите функцию greet(name), которая принимает имя в качестве аргумента и возвращает приветствие в формате "Привет, [name]!".
 // function greet(name) {
 //   return "Привет, " + name + "!";
@@ -247,12 +183,12 @@
 //   1. Оставьте только четные числа.
 //   2. Умножьте каждое оставшееся число на 3.
 //   3. Вычислите сумму получившихся чисел.
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const result = numbers
-  .filter((num) => num % 2 === 0)
-  .map((num) => num * 3)
-  .reduce((sum, num) => sum + num, 0);
-console.log(result);
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const result = numbers
+//   .filter((num) => num % 2 === 0)
+//   .map((num) => num * 3)
+//   .reduce((sum, num) => sum + num, 0);
+// console.log(result);
 
 // Roundmap
 // const obj = {};
